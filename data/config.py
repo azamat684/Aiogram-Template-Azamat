d8c6c02f53c9d7f0f442486938d1d83737fb1a61
Azamat Dosmukhambetov <122520658+azamat684@users.noreply.github.com>
2023-02-04 20:12:52 +0500

    Initialize kanal-bot

---
+++ data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = "5026156727:AAFnEy7vZ1BXVmF0Xs9-5V7U2MgZJQ07SSA"  # Bot token
+ADMINS = ["1343692719",]  # adminlar ro'yxati
+CHANNELS = ["-1001572570088", "@BekoDev"]
---
+++ keyboards/inline/__init__.py
@@ -0,0 +1,3 @@
+from . import manage
+from . import admin
+from . import subscription

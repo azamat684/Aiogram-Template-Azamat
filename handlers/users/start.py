04e694418601ad312769924ee4dcbfb1ff428a10
Azamat Dosmukhambetov <you@example.com>
2023-03-08 23:15:53 +0500

    Update

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,6 @@
 import sqlite3
 
 from aiogram import types
-from aiogram.types.force_reply import ForceReply
-from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
-
 from aiogram.dispatcher.filters.builtin import CommandStart
 from aiogram.dispatcher import FSMContext
 from data.config import ADMINS
@@ -15,29 +12,22 @@ from loader import dp, db, bot
 async def bot_start(message: types.Message,state: FSMContext):
     await state.finish()
     name = message.from_user.full_name
-    keyboard = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text="1-btn"),KeyboardButton("2-btn")]
-    ],resize_keyboard=True,input_field_placeholder="Birini tanlang")
     # Foydalanuvchini bazaga qo'shamiz
     try:
-        # salom = aiogram.types.force_reply.ForceReply.input_field_placeholder({"slom":salom})
-        # await message.answer("Slom",types.force_reply.ForceReply.input_field_placeholder="Salom")
-        db.add_user(id=message.from_user.id,
-                    name=name)
-        await types.force_reply.ForceReply()
-        await message.answer(f"Botimizga xush kelibsiz {name}\nBotni yangilayabmiz hozircha bot ishlamaydi bot ishga tushganda o'zimiz xabar beramiz etiboringgiz uchun raxmat 😊",reply_markup=keyboard)
+        db.add_user(id=message.from_user.id,name=name)
+        await message.answer(f"Botimizga xush kelibsiz {name}\nBotni yangilayabmiz hozircha bot ishlamaydi bot ishga tushganda o'zimiz xabar beramiz etiboringgiz uchun raxmat 😊")
         # Adminga xabar beramiz
         count = db.count_users()[0]
         if message.from_user.username is not None: 
-            msg = f"Bazaga yangi user qo'shildi\n🙎🏻‍♂️ Name: <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>\n🆔 ID si: <code>{message.from_user.id}</code>\n✉️ Username: @{message.from_user.username}\n\nBazada {count} ta foydalanuvchi bor."
-            await bot.send_message(chat_id=ADMINS[0], text=msg,parse_mode='HMTL')
+            msg = f"Bazaga yangi user qo'shildi\n🙎🏻‍♂️ Name: <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>\n🆔 ID si: <code>{message.from_user.id}</code>\n✉️ Username: @{message.from_user.username}\n\nBazada {count} ta foydalanuvchi bor"
+            await bot.send_message(chat_id=ADMINS[0], text=msg,parse_mode='HTML')
         else:
             msg = f"Bazaga yangi user qo'shildi\n🙎🏻‍♂️ Name: <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>\n🆔 ID si: <code>{message.from_user.id}</code>\n\nBazada {count} ta foydalanuvchi bor."
-            await bot.send_message(chat_id=ADMINS[0], text=msg,parse_mode='HMTL')
+            await bot.send_message(chat_id=ADMINS[0], text=msg,parse_mode='HTML')
 
     except sqlite3.IntegrityError as err:
         if message.from_user.username is not None:
             await bot.send_message(chat_id=ADMINS[0], text=f"🙎🏻‍♂️ <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a> bazaga oldin qo'shilgan\n✉️ Username: @{message.from_user.username}\n🆔 ID si: <code>{message.from_user.id}</code>",parse_mode='HTML')
         else:
             await bot.send_message(chat_id=ADMINS[0], text=f"🙎🏻‍♂️ <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a> bazaga oldin qo'shilgan\n🆔 ID si: <code>{message.from_user.id}</code>",parse_mode='HTML')
-        await message.answer(f"Botimizga xush kelibsiz {name}\nBotni yangilayabmiz hozircha bot ishlamaydi bot ishga tushganda o'zimiz xabar beramiz etiboringgiz uchun raxmat 😊",reply_markup=keyboard)
+        await message.answer(f"Botimizga xush kelibsiz {name}\nBotni yangilayabmiz hozircha bot ishlamaydi bot ishga tushganda o'zimiz xabar beramiz etiboringgiz uchun raxmat 😊")
---
+++ states/state.py
@@ -0,0 +1,3 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+class Reklama(StatesGroup):
+    optional_reklama = State()

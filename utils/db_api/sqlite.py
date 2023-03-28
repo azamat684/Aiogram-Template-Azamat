4daa8abe6ffd12665aff37be0d71c72552595225
Azamat Dosmukhambetov <you@example.com>
2023-03-28 23:37:28 +0500

    Initial commit

--- utils/db_api/sqlite.py
+++ utils/db_api/sqlite.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 class Database:
-    def __init__(self, path_to_db="C:/Users/Kazbek/Desktop/MY AIOGRAM BOTS/Aiogram-Template-Azamat/data/main.db"):
+    def __init__(self, path_to_db="main.db"):
         self.path_to_db = path_to_db
 
     @property

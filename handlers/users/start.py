e2fef539467c9ad1aa47f351e5eabe715c9bc428
Azamat Dosmukhambetov <122520658+azamat684@users.noreply.github.com>
2023-03-29 00:05:22 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -17,8 +17,9 @@ async def bot_start(message: types.Message,state: FSMContext):
         db.add_user(id=message.from_user.id,name=name)
         # Adminga xabar beramiz
         count = db.count_users()[0]
-        await bot.send_message(chat_id=ADMINS[0],text=f"Bazaga {name} qo'shildi\nBazada {count} ta foydalanuvchi bor")
+        msg = f"Bazaga {name} qo'shildi\nBazada {count} ta foydalanuvchi bor"
+        await bot.send_message(chat_id=ADMINS[0],text = msg)
         await message.answer(f"Xush kelibsiz {name}")
     except sqlite3.IntegrityError as err:
-        await bot.send_message(chat_id=ADMINS[0],text=f"Bazaga {name} qo'shildi\nBazada {count} ta foydalanuvchi bor")
+        await bot.send_message(chat_id=ADMINS[0],text = f"{name} bazaga oldin qo'shilgan")        
         await message.answer(f"Xush kelibsiz {name}")
